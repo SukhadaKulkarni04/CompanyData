68bcd13c1a30294f966a884bb511cc9fd7b2f618
SukhadaKulkarni04 <131472704+SukhadaKulkarni04@users.noreply.github.com>
2024-05-18 01:19:06 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,20 +8,20 @@ app = Flask(__name__)
 
 # Configure MongoDB
 CORS(app)
-app.config['MONGO_URI'] = 'mongodb+srv://Sukhada:ssk@12345@cluster0.3nfii4g.mongodb.net/'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017'
 mongo = PyMongo(app)
 
 # Define the company collection
 companies_collection = mongo.db.companydata
 
 # API endpoint to fetch all companies
-@app.route("https://companydata-production.up.railway.app/api/companies", methods=['GET'])
+@app.route("/api/companies", methods=['GET'])
 def get_companies():
     companies = list(companies_collection.find({}, {'_id': 0}))
     return jsonify(companies)
 
 # API endpoint to search companies by name
-@app.route("https://companydata-production.up.railway.app/api/companies", methods=["GET"])
+@app.route("/api/companies", methods=["GET"])
 def search_companies():
     query = request.args.get('q')
     companies = list(companies_collection.find({}, {'_id': 0}))
