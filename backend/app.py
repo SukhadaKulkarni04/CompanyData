2361fd75994c55c935faa263358c046cc92e1ab0
SukhadaKulkarni04 <131472704+SukhadaKulkarni04@users.noreply.github.com>
2024-05-17 18:19:58 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Configure MongoDB
 CORS(app)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/company'
+app.config['MONGO_URI'] = 'mongodb+srv://sukhadakulkarni9214:i2XIcTIKEvLN1hLd@company.glphkoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongo = PyMongo(app)
 
 # Define the company collection
@@ -28,4 +28,4 @@ def search_companies():
     return jsonify(companies)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
